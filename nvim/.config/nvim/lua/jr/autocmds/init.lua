f8e016eb474c4d910146ed1b2923752755d4e7ec
Justin Rassier <jrassier@adventhp.com>
2021-12-16 13:34:41 -0600

    whoops forgot some file

---
+++ nvim/.config/nvim/lua/jr/autocmds/init.lua
@@ -0,0 +1,8 @@
+local M = {}
+function M.autocmd(event, triggers, operations)
+  local cmd = string.format("autocmd %s %s %s", event, triggers, operations)
+ vim.cmd(cmd)
+end
+
+
+M.autocmd("ColorScheme",  "*",   "lua require('jr.colors').setItalics()")

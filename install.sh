df1008df33f69cdec92609af9b8d70d93e613426
Justin Rassier <jrassier@adventhp.com>
2024-03-10 11:23:37 -0500

    setting up stuff on new computer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 DOTFILES=$HOME/dotfiles
 #!/usr/bin/env zsh
-STOW_FOLDERS="nvim,kitty,zsh,tmux"
+STOW_FOLDERS="nvim,kitty,zsh,tmux,tmuxinator"
 
 
 # Prerequisites 
@@ -12,7 +12,8 @@ STOW_FOLDERS="nvim,kitty,zsh,tmux"
 
 
 #nvm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 
 # HOMEBREW
@@ -24,24 +25,22 @@ STOW_FOLDERS="nvim,kitty,zsh,tmux"
 # brew install neovim
 # brew tap homebrew/cask-fonts && brew install font-Fira-Code-nerd-font
 # brew install stow
-# brew install jesseduffield/lazygit/lazygit
-# brew install tmuxinator
-# brew install ripgrep
+ # brew install jesseduffield/lazygit/lazygit
+ # brew install tmuxinator
+ # brew install ripgrep
 
 # Install packer
 
 # git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 # ~/.local/share/nvim/site/pack/packer/start/packer.nvim
- 
 
- # nvim-lsp reference  (https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md)
+# Tmux Plugin Manager
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+# Install z for faster directory navigation
+# git clone git@github.com:rupa/z.git git/z
  
- #  Install language servers needed from npm
-# npm install -g typescript typescript-language-server
-# npm install -g @angular/language-server@latest
-# npm install -g @tailwindcss/language-server
-# npm install -g vscode-langservers-extracted
-# npm install -g cspell@latest
 
 
 pushd $DOTFILES
